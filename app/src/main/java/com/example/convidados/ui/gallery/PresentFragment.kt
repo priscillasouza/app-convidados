9d315c2a3f469b1810dfea5006b7e9859a865db9
priscilla <priscillasilva-24@hotmail.com>
2021-09-09 22:15:05 -0300

    create registration layout

--- app/src/main/java/com/example/convidados/ui/gallery/PresentFragment.kt
+++ app/src/main/java/com/example/convidados/ui/gallery/PresentFragment.kt
@@ -8,12 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.convidados.R
 import com.example.convidados.databinding.FragmentGalleryBinding
 
-class GalleryFragment : Fragment() {
+class PresentFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var presentViewModel: PresentViewModel
     private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,14 +24,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        presentViewModel =
+            ViewModelProvider(this).get(PresentViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+        presentViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
